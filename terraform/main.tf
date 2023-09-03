fc095913c9ec2639b0d06c19c37c67c2fb2415fe
Luke Morton <lukemorton@tutanota.com>
2023-09-03 17:16:05 -0500

    fix(iac): fixed the terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0.2"
+      version = "~> 3.0.2"
     }
   }
 
@@ -16,20 +16,20 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "odin-recipes" {
-  name = "odin-recipes"
+  name     = "odin-recipes"
   location = "northcentralus"
 }
 
-resource "azurerm_storage_account" "odin-recipes-01929317" {
-  name = "odin-recipes-01929317"
-  location = "northcentralus"
-  resource_group_name = azurerm_resource_group.odin-recipes.name
-  account_tier = "Standard"
+resource "azurerm_storage_account" "odinrecipes01929317" {
+  name                     = "odinrecipes01929317"
+  location                 = "northcentralus"
+  account_kind             = "StorageV2"
+  resource_group_name      = azurerm_resource_group.odin-recipes.name
+  account_tier             = "Standard"
   account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "web" {
-  name = "$web"
-  storage_account_name = azurerm_storage_account.odin-recipes-01929317.name
-  container_access_type = "blob"
+  access_tier              = "Hot"
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "404.html"
+  }
 }
